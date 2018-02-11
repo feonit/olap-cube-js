c2826d60f82732bd157bd437db0b77678064e8e1
feonit <feonitu@yandex.ru>
2018-02-11 22:42:52 +0300

    test cover

--- spec/spec.js
+++ spec/spec.js
@@ -1,3 +1,6 @@
-require('./function_spec.js')
-require('./schema_spec.js')
-require('./etalon_spec.js')
+// require('./function_spec.js')
+// require('./schema_spec.js')
+// require('./etalon_spec.js')
+import './function_spec.js'
+import './schema_spec.js'
+import './etalon_spec.js'
