df7da74b85188cd8a325c43cb7aaa37a88bae15b
feonit <feonitu@yandex.ru>
2018-03-18 20:04:15 +0300

    update config

--- config/webpack.config.app.js
+++ config/webpack.config.app.js
@@ -1,14 +1,17 @@
 const path = require('path');
+const webpack = require("webpack");
+const fs = require("fs");
 
 module.exports = {
     entry: {
-        cube: path.resolve(__dirname, '../src/Cube.js'),
+        "cube": path.resolve(__dirname, '../src/Cube.js'),
+        "cube.min": path.resolve(__dirname, '../src/Cube.js'),
     },
     output: {
         path: path.resolve(__dirname, '../dist'),
         filename: '[name].js',
         library: "Cube",
-        libraryTarget: "var"
+        libraryTarget: "umd"
     },
     module: {
         rules: [
@@ -17,5 +20,12 @@ module.exports = {
                 loader: "babel-loader"
             }
         ]
-    }
+    },
+    plugins: [
+	    new webpack.BannerPlugin(fs.readFileSync('./LICENSE', 'utf8')),
+	    new webpack.optimize.UglifyJsPlugin({
+		    include: /\.min\.js$/,
+		    minimize: true
+	    })
+    ]
 };
